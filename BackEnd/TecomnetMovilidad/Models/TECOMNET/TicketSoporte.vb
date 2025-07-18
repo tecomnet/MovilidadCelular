6eeb158230c1c750fd39d1a380ac0110be09a378
tecomnet <e.escandon@tecomnet.mx>
2025-07-17 22:46:28 -0600

    Modelos

---
+++ BackEnd/TecomnetMovilidad/Models/TECOMNET/TicketSoporte.vb
@@ -0,0 +1,13 @@
+﻿Imports Models.TECOMNET.Enumeraciones
+
+Namespace TECOMNET
+    Public Class TicketSoporte
+        Public Property TicketSoporteId As Integer
+        Public Property ClienteId As Integer
+        Public Property FechaCreacion As Date
+        Public Property Asunto As String
+        Public Property Descripcion As String
+        Public Property Estatus As EstatusTicket
+        Public Property FechaCierre As Date?
+    End Class
+End Namespace
---
+++ BackEnd/TecomnetMovilidad/Models/TECOMNET/Usuario.vb
@@ -0,0 +1,23 @@
+﻿Imports Models.TECOMNET.Enumeraciones
+Namespace TECOMNET
+
+    Public Class Usuario
+        Public Property UsuarioId As Integer
+        Public Property NombreUsuario As String
+        Public Property Nombre As String
+        Public Property ContrasenaHash As String
+        Public Property TipoUsuario As TipoUsuario
+        Public Property FechaAlta As DateTime
+        Public Property UltimoLogin As DateTime?
+        Public Property FechaBaja As DateTime?
+        Public Property Activo As Boolean
+
+        'Public Function ValidarContraseña(contraseña As String) As Boolean
+        '    Return BCrypt.Net.BCrypt.Verify(contraseña, ContraseñaHash)
+        'End Function
+
+        'Public Sub SetContraseña(contraseña As String)
+        '    ContraseñaHash = BCrypt.Net.BCrypt.HashPassword(contraseña)
+        'End Sub
+    End Class
+End Namespace
