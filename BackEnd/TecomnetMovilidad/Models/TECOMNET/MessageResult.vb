6644b294c298423b4d3ea28012e57dc02024002c
tecomnet <e.escandon@tecomnet.mx>
2025-08-15 13:04:18 -0600

    Cambios Modelos Result

---
+++ BackEnd/TecomnetMovilidad/Models/TECOMNET/MessageResult.vb
@@ -0,0 +1,8 @@
+﻿Imports Models.TECOMNET.Enumeraciones
+
+Namespace TECOMNET
+    Public Class MessageResult
+        Public Property ErrorID As TipoErroresAPI
+        Public Property JSON As String
+    End Class
+End Namespace
