0353d7c8c9375916e5e6042a6ad836c8060d5d4e
Tecomnet <e.escandon@tecomnet.mx>
2025-09-10 15:23:30 -0600

    integracion de inactividad

--- movilidad_celulares/lib/utils/session_manager.dart
+++ movilidad_celulares/lib/utils/session_manager.dart
@@ -3,14 +3,11 @@ import 'package:shared_preferences/shared_preferences.dart';
 class SessionManager {
   static const _keyLoggedIn = "isLoggedIn";
   static const _keyUser = "savedUser";
-  static const _keyLoginTime = "loginTime"; 
 
-  static const sessionDuration = 1; 
-
+  // Guardar sesión
   static Future<void> login(String username, {bool remember = false}) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setBool(_keyLoggedIn, true);
-    await prefs.setInt(_keyLoginTime, DateTime.now().millisecondsSinceEpoch); 
     if (remember) {
       await prefs.setString(_keyUser, username);
     } else {
@@ -18,37 +15,22 @@ class SessionManager {
     }
   }
 
+  // Cerrar sesión
   static Future<void> logout() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.remove(_keyLoggedIn);
-    await prefs.remove(_keyLoginTime);
-  }
+  final prefs = await SharedPreferences.getInstance();
+  // Borra solo la sesión, pero conserva el usuario guardado si existía
+  await prefs.remove(_keyLoggedIn);
+}
 
+  // Validar si hay sesión activa
   static Future<bool> isLoggedIn() async {
     final prefs = await SharedPreferences.getInstance();
-    final loggedIn = prefs.getBool(_keyLoggedIn) ?? false;
-
-    if (!loggedIn) return false;
-
-    final loginTime = prefs.getInt(_keyLoginTime) ?? 0;
-    final now = DateTime.now().millisecondsSinceEpoch;
-    final diffMinutes = (now - loginTime) ~/ 60000;
-
-    if (diffMinutes > sessionDuration) {
-      await logout(); 
-      return false;
-    }
-    return true;
+    return prefs.getBool(_keyLoggedIn) ?? false;
   }
 
+  // Obtener usuario guardado
   static Future<String?> getUser() async {
     final prefs = await SharedPreferences.getInstance();
     return prefs.getString(_keyUser);
   }
-
-  static Future<void> refreshSession() async {
-  final prefs = await SharedPreferences.getInstance();
-  await prefs.setInt(_keyLoginTime, DateTime.now().millisecondsSinceEpoch);
-}
-
 }
